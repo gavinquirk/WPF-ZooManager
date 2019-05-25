bfe8c116f6560b8024cbf2d19a7597bf47e4a526
Gavin Quirk <gavinquirk@gmail.com>
2019-05-24 20:56:28 -0700

    db connection, ListBox filled with zoos from Zoo table

--- WPF-ZooManager/MainWindow.xaml.cs
+++ WPF-ZooManager/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WPF_ZooManager
 {
@@ -21,12 +23,43 @@ namespace WPF_ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
+
         public MainWindow()
         {
             InitializeComponent();
 
             // Connection String Format: projectName.Properties.Settings.dataSourceString
-            string connectionString = ConfigurationManager.ConnectionStrings["WPF-ZooManager.Properties.Settings.gqdbConnectionString"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZooManager.Properties.Settings.gqdbConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            ShowZoos();
+        }
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooTable);
+
+                    // Which information of the Table in DataTable should be shown in the ListBox
+                    listZoos.DisplayMemberPath = "Location";
+                    // Which Value should be delivered when an Item from the ListBox is selected
+                    listZoos.SelectedValuePath = "Id";
+                    // The Reference to the Data the ListBox should populate
+                    listZoos.ItemsSource = zooTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
         }
     }
 }
